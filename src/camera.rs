53d0c4c47836bf979990ef7227e7243d000795ac
Albert Bennett <blubberblat.bennett@gmail.com>
2024-08-11 14:21:40 -0400

    Finished camera struct refactor

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,12 @@
+use crate::hit::World;
 use crate::vec3::Vec3;
+use crate::ray::Ray;
+use crate::hit::Hittable;
+use crate::geometry::*;
+use std::fs;
 
-
-struct Camera {
+#[derive(Debug,Clone)]
+pub struct Camera {
 
 	pub image_width: i64,
     pub aspect_ratio: f64,
@@ -30,7 +35,7 @@ impl Camera {
 			viewport_height: view_height,
 		
 			image_height: 0,
-			img_data: Image::new(), 
+			img_data: Image::new(0,0), 
 			viewport_width: 0.0,
 			camera_center: Vec3::zero(),
 			viewport_hor: Vec3::zero(),
@@ -44,6 +49,7 @@ impl Camera {
 	pub fn init(&mut self) {
 		self.viewport_height = 2.0;
 		self.focal_length = 1.0;
+		self.image_height = (self.image_width as f64/self.aspect_ratio) as i64;
 		self.viewport_width = self.viewport_height * (self.image_width as f64/self.image_height as f64);
 		self.camera_center = Vec3(0.0,0.0,0.0);
 	
@@ -54,21 +60,58 @@ impl Camera {
 		
 		self.vw_ul_corner = self.camera_center- Vec3(0.0,0.0,self.focal_length) - self.viewport_hor/2.0 - self.viewport_ver/2.0;
 		self.cntr_frst_px = self.vw_ul_corner + ((self.pixel_delta_hor + self.pixel_delta_ver) *0.5);
-	
+		self.img_data.h = self.image_height;
+		self.img_data.w = self.image_width;
 	}
-	pub fn render(&mut self) {
-
+	pub fn render(&mut self, w: &World) {
+		for j in 0..self.image_height {
+			for i in 0..self.image_width {
+				let px_cntr = self.cntr_frst_px + (self.pixel_delta_hor * i as f64) + (self.pixel_delta_ver * j as f64);
+				let ray_dir = px_cntr - self.camera_center;
+				let px_ray = Ray::new(self.camera_center,ray_dir);
+	
+				let px_color = Self::color_at(px_ray, &w);
+				self.img_data.data.push(px_color);
+			}   
+		}
+	}
+	fn color_at(r: Ray, w: &World) -> Vec3 {
+		let possible_hit = w.hit(r, Interval::new(0.0, f64::INFINITY));
+		match possible_hit {
+			Some(h) => {
+				let normal = h.norm;
+				return  (normal+1.0) *0.5;
+			}
+			None => {
+				let unit_dir = r.dir.normalize();
+				let a = 0.5 * (unit_dir.1 + 1.0);
+				return (Vec3(1.0,1.0,1.0)*(1.0-a)) + Vec3(0.3,0.6,1.0) * a;
+			}
+		}
+	}
+	pub fn image(self) -> Image {
+		self.img_data
 	}
 }
 
-struct Image {
+#[derive(Debug,Clone)]
+pub struct Image {
+	h: i64,
+	w: i64,
 	data: Vec<Vec3>
 }
 
 impl Image {
-	pub fn new() -> Self {
+	pub fn new(height: i64, width: i64) -> Self {
 		Self {
+			h: height,
+			w: width,
 			data: Vec::new()
 		}
+	}
+	pub fn out_ppm(self,file_name: &str) {
+		let mut out :Vec<String> = vec![format!("P3\n{} {}\n255",self.w,self.h)];
+		self.data.iter().for_each(|p| out.push(p.write_color()));
+		fs::write(file_name,out.join("\n")).unwrap();
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,18 @@
-use std::fs;
-use geometry::{Sphere, Triangle,Interval};
-use ray::Ray;
+use geometry::{Sphere, Triangle};
 use vec3::Vec3;
-use hit::{Hittable,World};
+use hit::World;
+use camera::Camera;
 mod vec3;
 mod ray;
 mod geometry;
 mod hit;
 mod camera;
 
-fn color_at(r: Ray, w: &World) -> Vec3 {
-    let possible_hit = w.hit(r, Interval::new(0.0, f64::INFINITY));
-    match possible_hit {
-        Some(h) => {
-            let normal = h.norm;
-            return  (normal+1.0) *0.5;
-        }
-        None => {
-            let unit_dir = r.dir.normalize();
-            let a = 0.5 * (unit_dir.1 + 1.0);
-            return (Vec3(1.0,1.0,1.0)*(1.0-a)) + Vec3(0.3,0.6,1.0) * a;
-        }
-    }
-    
-}
-
 fn main() {
 
-    let image_width = 400;
-    let aspect_ratio = 16.0/9.0;
-    let image_height = (image_width as f64/aspect_ratio) as i32;
-
-    let mut img_data :Vec<String> = vec![format!("P3\n{image_width} {image_height}\n255\n")];
-
-    let viewport_height = 2.0;
-    let focal_length = 1.0;
-    let viewport_width = viewport_height * (image_width as f64/image_height as f64);
-    let camera_center = Vec3(0.0,0.0,0.0);
-
-    let viewport_hor = Vec3(viewport_width,0.0,0.0);
-    let viewport_ver = Vec3(0.0,-viewport_height,0.0);
-    let pixel_delta_hor = viewport_hor/image_width as f64;
-    let pixel_delta_ver = viewport_ver/image_height as f64;
-    
-    let vw_ul_corner = camera_center- Vec3(0.0,0.0,focal_length) - viewport_hor/2.0 - viewport_ver/2.0;
-    let cntr_frst_px = vw_ul_corner + ((pixel_delta_hor + pixel_delta_ver) *0.5);
+    //Init camera 
+    let mut cam = Camera::new(400, 16.0/9.0,1.0,2.0);
+    cam.init();
 
     //Init World
     let mut world = World::new();
@@ -52,17 +20,9 @@ fn main() {
     world.add_object(Box::new(Sphere::new(Vec3(0.0,-100.5,-1.0), 100.0)));
     world.add_object(Box::new(Triangle::new(Vec3(-2.0, -1.0, -2.0), Vec3(2.0,-1.0,-2.0), Vec3(0.0,2.0,-2.0))));
 
-    //Write pixel values
-    for j in 0..image_height {
-        for i in 0..image_width {
-            let px_cntr = cntr_frst_px + (pixel_delta_hor * i as f64) + (pixel_delta_ver * j as f64);
-            let ray_dir = px_cntr - camera_center;
-            let px_ray = Ray::new(camera_center,ray_dir);
+    //Render image and write to ppm
+    //TODO: implement support for other image formats
+    cam.render(&world);
+    cam.image().out_ppm("out.ppm");
 
-            let px_color = color_at(px_ray, &world);
-            img_data.push(px_color.write_color());
-        }   
-    }
-    //Write to file
-    fs::write("out.ppm", img_data.join("\n")).unwrap();
 }
